4cbd19f51abc7438cccf4a4be0505edb1dbf1dd2
astradae <astradaezequiel.tpi@gmail.com>
2022-02-16 10:40:12 -0300

    Objects

---
+++ Objects.ts
@@ -0,0 +1,29 @@
+const obj = {
+    firstName: "Eze",
+    lastName: "Astr",
+    phone: "111-111-111",
+    age: 1000,
+    gender: false
+}
+
+const obj2 : {
+    firstName: string;
+    lastName: string;
+    phone: string;
+    age: number;
+    gender: boolean
+} = {
+    firstName: "",
+    lastName: "",
+    phone: "",
+    age: 1000,
+    gender: true
+}
+
+obj2.age = 8000;
+obj2.age = "qwert"
+
+obj2.name 
+
+obj2.phone.length
+obj2.age.length
