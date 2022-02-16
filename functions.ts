7f5da67876d02ff0912e95e30f2b444b8270a21c
astradae <astradaezequiel.tpi@gmail.com>
2022-02-16 11:22:18 -0300

    Functions

---
+++ functions.ts
@@ -0,0 +1,62 @@
+const add = (num1, num2) => {
+    return num1 + num2
+}
+add(true, [])
+
+const add2 = (num1, num2) => {
+    return num1 + num2
+}
+add2(true)
+
+
+const subtract = (num1: number, num2: number) => {
+    return num1 - num2
+}
+subtract(4,4)
+subtract(4,[])
+subtract(4,false)
+subtract(4,4).length
+
+const compare = (num1: number, num2: number) => {
+    // Logic
+    // Logic
+    // Logic
+    // Logic
+    // Logic
+    // Logic
+    return num1 > num2
+}
+
+const compare2 = (num1: number, num2: number) => {
+    // Logic
+    // Logic
+    // Logic
+    // Logic
+    // Logic
+    // Logic
+    return num1 + num2
+}
+
+const compare3 = (num1: number, num2: number): boolean => {
+    // Logic
+    // Logic
+    // Logic
+    // Logic
+    // Logic
+    // Logic
+    return num1 > num2
+}
+
+const compare4 = (num1: number, num2: number): boolean => {
+    // Logic
+    // Logic
+    // Logic
+    // Logic
+    // Logic
+    // Logic
+    return num1 + num2
+}
+
+const log = (): void  => {
+    console.log("hello")
+}
