f0d8073e8d624b930115aa5d04abb5a07e66252a
astradae <astradaezequiel.tpi@gmail.com>
2022-02-16 10:08:08 -0300

    Built-in Types

---
+++ index.ts
@@ -0,0 +1,25 @@
+// NUMBER TYPE
+let age = 24;
+
+let weight: number = 170;
+
+weight = 180;
+weight = 160;
+weight = true;
+
+age = 25;
+age = 23;
+age = "25"
+
+// TYPE STRING
+let myName = "Ezequiel"
+let myDogName: string = "Mara"
+
+myName = true;
+myName = "Ezequiel".length
+myName = "Ezeuqiel".length === 0
+
+// TYPE BOOLEAN
+let isAwesome = false;
+let isAwesome2 = "Ezeuqiel".length === 0
+let isBoring: boolean = true
