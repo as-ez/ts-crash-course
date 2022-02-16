1eb782cdb8e85c4bf011d1fbcb518b2412a2aae7
astradae <astradaezequiel.tpi@gmail.com>
2022-02-16 11:06:33 -0300

    Arrays

---
+++ array.ts
@@ -0,0 +1,33 @@
+const arrayOfString = ["string1", "string2", "string3"]
+arrayOfString.push("string4")
+arrayOfString.push(true)
+
+
+const arrayOfBooleans: boolean[] = [true, true, false]
+arrayOfBooleans.push(true)
+
+const arrayOfNumbers = [1,2,3,4]
+
+const array = []
+array.push(true)
+array.push("")
+array.push(5)
+
+const array2: string[] = []
+array2.push(true)
+array2.push("")
+array2.push(5)
+
+let arrayOfObject: {
+    name: string;
+    age: number;
+    gender: boolean
+}[] = []
+
+arrayOfObject.push({
+    name: "",
+    age: 2,
+    gender: true
+})
+
+const arrayOfArrays = [["", "", ""]]
